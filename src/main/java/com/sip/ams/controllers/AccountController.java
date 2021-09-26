da4d300250d9dcff3e95d357d85df781f94f7a40
azzachihi <chihiazza@gmail.com>
2021-09-26 15:36:46 +0200

    projet gestion de stock deploiement heroku8

--- src/main/java/com/sip/ams/controllers/AccountController.java
+++ src/main/java/com/sip/ams/controllers/AccountController.java
@@ -1,5 +1,9 @@
 package com.sip.ams.controllers;
+
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,97 +13,118 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.sip.ams.entities.Role;
 import com.sip.ams.entities.User;
 import com.sip.ams.repositories.RoleRepository;
 import com.sip.ams.repositories.UserRepository;
-import java.util.*;
+import com.sip.ams.entities.Role;
 
+// import spécifiques au mail
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import javax.mail.MessagingException;
 import java.io.IOException;
 
+
 @Controller
 @RequestMapping("/accounts/")
+
 public class AccountController {
-	private final UserRepository userRepository;
+
 	private final RoleRepository roleRepository;
-
-	@Autowired
-	private JavaMailSender javaMailSender;
-	
+	private final UserRepository userRepository;
 	
 	@Autowired
-	public AccountController(UserRepository userRepository, RoleRepository roleRepository) {
-		this.userRepository = userRepository;
-		this.roleRepository = roleRepository;
-	}
+    private JavaMailSender javaMailSender;
 
+	
+	@Autowired
+    public AccountController(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+	
 	@GetMapping("list")
-	public String listUsers(Model model) {
-		List<User> users = (List<User>) userRepository.findAll();
-		long nbr = userRepository.count();
-		if (users.size() == 0)
-			users = null;
-		model.addAttribute("users", users);
-		model.addAttribute("nbr", nbr);
-		return "user/listUsers";
-	}
+    public String listUsers(Model model) {
+    	
+    	List<User> users = (List<User>) userRepository.findAll();
+    	long nbr =  userRepository.count();
+    	if(users.size()==0)
+    		users = null;
+        model.addAttribute("users", users);
+        model.addAttribute("nbr", nbr);
+        return "user/listUsers";
+    }
 	
-	@GetMapping("enable/{id}/{email}")
-	//@ResponseBody
-	public String enableUserAcount(@PathVariable ("id") int id, @PathVariable ("email") String email) {
-	sendEmail(email, true);
-	User user = userRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid User Id:" + id));
-	user.setActive(1);
-	userRepository.save(user);
-	return "redirect:../../list";
-	}
-	
-	
-	
-	@GetMapping("disable/{id}/{email}")
-	//@ResponseBody
-	public String disableUserAcount(@PathVariable ("id") int id,
-	@PathVariable ("email") String email) {
-	sendEmail(email, false);
-	User user = userRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid User Id:" + id));
-	user.setActive(0);
-	userRepository.save(user);
-	return "redirect:../../list";
-	}
 	
 	@PostMapping("updateRole")
 	//@ResponseBody
 	public String UpdateUserRole(@RequestParam ("id") int id,
-	@RequestParam ("newrole")String newRole
-	) {
-	User user = userRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid User Id:" + id));
-	Role userRole = roleRepository.findByRole(newRole);
-	user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
-	userRepository.save(user);
-	return "redirect:list";
-	}
+			@RequestParam ("newrole")String newRole
+			) {
+    	
+		 User user = userRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid User Id:" + id));
+	     
+		 Role userRole = roleRepository.findByRole(newRole);
+		 
+	     user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+	     
+	     userRepository.save(user);
+    	return "redirect:list";
+    }
+	
+	
+	@GetMapping("enable/{id}/{email}")
+	//@ResponseBody
+    public String enableUserAcount(@PathVariable ("id") int id, 
+    		@PathVariable ("email") String email) {
+    	
+		 sendEmail(email, true);
+		 User user = userRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid User Id:" + id));
+	     user.setActive(1);
+	     userRepository.save(user);
+    	return "redirect:../../list";
+    }
+	
+	@GetMapping("disable/{id}/{email}")
+	//@ResponseBody
+	public String disableUserAcount(@PathVariable ("id") int id, 
+    		@PathVariable ("email") String email) {
+		 sendEmail(email, false);
+		 User user = userRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Invalid User Id:" + id));
+	     user.setActive(0);
+	     userRepository.save(user);
+    	return "redirect:../../list";
+    }
+	
 	
 	void sendEmail(String email, boolean state) {
-		SimpleMailMessage msg = new SimpleMailMessage();
-		msg.setTo(email);
-		
-		if(state == true)
-		{
-		msg.setSubject("Account Has Been Activated");
-		msg.setText("Hello, Your account has been activated. "
-		+
-		"You can log in : http://127.0.0.1:81/login"
-		+ " \n Best Regards!");
-		}
-		else
-		{
-		msg.setSubject("Account Has Been disactivated");
-		msg.setText("Hello, Your account has been disactivated.");
-		}
-		javaMailSender.send(msg);
-		}
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        
+        msg.setTo(email);
+        if(state == true)
+        {
+        msg.setSubject("Account Has Been Activated");
+        msg.setText("Hello, Your account has been activated. "
+        		+ 
+        		"You can log in : http://127.0.0.1:81/login"
+        		+ " \n Best Regards!");
+        }
+        else
+        {
+        	msg.setSubject("Account Has Been disactivated");
+            msg.setText("Hello, Your account has been disactivated.");
+        }
+        javaMailSender.send(msg);
+
+    }
+	
+    
 }
+
+
+
+ 
+
+
